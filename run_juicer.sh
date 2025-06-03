2bc249125dc9c0f4728164b62a3cd882289ef918
Aaryan Bhatia <abhatia@pc-t7-025.mpipz.mpg.de>
2025-06-03 16:31:22 +0200

    fixed run_juicer to be actual file instead of link

--- run_juicer.sh
+++ run_juicer.sh
@@ -1,1 +1,45 @@
-/home/abhatia/../../netscratch/dep_mercier/grp_marques/Aaryan/tasks/HiC_QC/6181M/run_juicer.sh
+#!/bin/bash
+
+# running juicer for qc purposes
+#
+# 1. u make a JSD
+JSD="/netscratch/dep_mercier/grp_marques/Aaryan/tasks/HiC_QC/6181M"
+
+# 2. u put ur genome (contig level) in the references folder
+#
+REF=${JSD}/references/reference_scorpioides.fa
+
+# 3. u make a fastq folder containing the HiC reads in question
+mkdir -p ${JSD}/HiC_work/fastq
+
+# 4. u soft link the scripts juicer needs to run
+ln -s /netscratch/dep_mercier/grp_marques/Aaryan/methods/myJuicerDir/juicer/CPU ./scripts 
+
+
+prefix="D_scorp"
+
+
+cd ${JSD}/references
+
+bwa index ${REF}
+
+samtools faidx ${REF}
+
+awk '{print $1"\t"$2}' ${REF}.fai > ${prefix}.chrom.sizes
+
+# Run juicer
+#
+cd $JSD
+
+/netscratch/dep_mercier/grp_marques/Aaryan/methods/myJuicerDir/scripts/juicer.sh \
+  -g $prefix \
+  -d $JSD/HiC_work \
+  -p $JSD/references/${prefix}.chrom.sizes \
+  -z $REF \
+  -D $JSD \
+  -t 32 \
+  --assembly 0
+
+
+# can run 3ddna from here but im not interested in it right now
+
